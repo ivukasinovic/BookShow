317621c54ca348f21e1facfec12fc4bf633b58ae
ivukasinovic <fdigitalna@gmail.com>
2017-11-08 00:04:59 +0100

    IntelliJ test commit

--- src/main/java/bookshow/funpage/FunPageController.java
+++ src/main/java/bookshow/funpage/FunPageController.java
@@ -1,14 +1,23 @@
 package bookshow.funpage;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class FunPageController {
 
+    private final FunPageService funPageService;
+
+    @Autowired
+    public FunPageController(FunPageService funPageService) {
+        this.funPageService = funPageService;
+    }
+
 	@RequestMapping("/funpage")
 	public String hello() {
-		return "Welcome to fun page";
+		return  funPageService.hello();
 	}
 
 }
