be69231f5176acff015266222ab2da701060508e
ivukasinovic <fdigitalna@gmail.com>
2017-11-08 00:16:32 +0100

    IntelliJ test commit

---
+++ src/main/java/bookshow/funpage/FunPageService.java
@@ -0,0 +1,15 @@
+package bookshow.funpage;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author IvanV
+ */
+@Service
+public class FunPageService {
+
+    String hello()
+    {
+        return "Welcome to fun page";
+    }
+}
