aeae34a322896fda56f89eedd81e525014d578f7
ivukasinovic <fdigitalna@gmail.com>
2017-11-07 23:06:03 +0100

    First commit

---
+++ src/main/java/com/bookshow/BookShowApplication.java
@@ -0,0 +1,12 @@
+package com.bookshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookShowApplication.class, args);
+	}
+}
