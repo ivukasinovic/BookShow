e7f0454a5dfef5591850d051841d85c46b9b831a
ivukasinovic <fdigitalna@gmail.com>
2018-01-31 18:44:02 +0100

    fanpage edited

--- client/src/app/models/prop.ts
+++ client/src/app/models/prop.ts
@@ -2,12 +2,24 @@ export class UsedProp {
   id: number;
   title: string;
   description: string;
-  activeUntil: any;
+  dateCreated: string;
+  activeUntil: string;
   status: string;
+  acceptedBid: boolean;
+  adminFan: number;
+  registeredUser: number;
+  image: string;
+  createdDate: string;
 }
 export class NewProp {
   id: number;
   title: string;
   description: string;
+  dateCreated: string;
   price: number;
+  adminFan: number;
+  registeredUser: number;
+  show: number;
+  image: string;
+  createdDate: string;
 }
