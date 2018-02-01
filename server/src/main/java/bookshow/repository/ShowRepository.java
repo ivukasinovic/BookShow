df3da2549c6fca051182c9d92fce25612b67a24b
ivukasinovic <fdigitalna@gmail.com>
2018-02-01 03:40:33 +0100

    fanpage edit

---
+++ server/src/main/java/bookshow/repository/ShowRepository.java
@@ -0,0 +1,20 @@
+package bookshow.repository;
+
+import bookshow.model.Show;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Created by Ivan V. on 01-Feb-18
+ */
+public interface ShowRepository extends JpaRepository<Show,Long> {
+    List<Show> findAll();
+
+    Show findOne(Long id);
+
+    Show save(Show show);
+
+    void delete(Long id);
+
+}
